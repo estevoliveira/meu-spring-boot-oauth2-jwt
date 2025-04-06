a6ca1f948e6cbe64648815f0954c1f4559e9c483
Estevao <estevoliveira1@gmail.com>
2025-04-05 23:38:02 -0300

    security checklist not tested

--- password-grant/spring-boot-3-4-3-noauth/src/main/java/com/devsuperior/demo/DemoApplication.java
+++ password-grant/spring-boot-3-4-3-noauth/src/main/java/com/devsuperior/demo/DemoApplication.java
@@ -1,13 +1,26 @@
 package com.devsuperior.demo;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
-public class DemoApplication {
+public class DemoApplication implements CommandLineRunner {
+
+	@Autowired
+	private PasswordEncoder encoder;
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println(encoder.encode("123456"));
+
+		boolean result =  encoder.matches("123456","$2a$10$CJ6gKMzi4grmaLL15oP5XONKr5iTTe27fC74GgH/WE/e/4ZShNIc.");
+		System.out.println("Senha esta correta? "+result);
+	}
 }
--- password-grant/spring-boot-3-4-3-noauth/src/main/resources/import.sql
+++ password-grant/spring-boot-3-4-3-noauth/src/main/resources/import.sql
@@ -1,8 +1,8 @@
 INSERT INTO tb_role (authority) VALUES ('ROLE_OPERATOR');
 INSERT INTO tb_role (authority) VALUES ('ROLE_ADMIN');
 
-INSERT INTO tb_user (name, email, password) VALUES ('Alex', 'alex@gmail.com', '123456');
-INSERT INTO tb_user (name, email, password) VALUES ('Maria', 'maria@gmail.com', '123456');
+INSERT INTO tb_user (name, email, password) VALUES ('Alex', 'alex@gmail.com', '$2a$10$CJ6gKMzi4grmaLL15oP5XONKr5iTTe27fC74GgH/WE/e/4ZShNIc.');
+INSERT INTO tb_user (name, email, password) VALUES ('Maria', 'maria@gmail.com', '$2a$10$CJ6gKMzi4grmaLL15oP5XONKr5iTTe27fC74GgH/WE/e/4ZShNIc.');
 
 INSERT INTO tb_user_role (user_id, role_id) VALUES (1, 1);
 INSERT INTO tb_user_role (user_id, role_id) VALUES (2, 1);
